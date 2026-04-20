88c1ae07ee8cf4fbd86cab0473042ad838d49a4a
Gianfranco Cito <citovich1975@gmail.com>
2026-04-20 10:01:35 +0200

    creati model per clients,projects,tasks

--- database/migrations/2026_04_20_074248_create_client_project_table.php
+++ database/migrations/2026_04_20_074248_create_client_project_table.php
@@ -6,20 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('client_project', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('client_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('project_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+
+            $table->unique(['client_id', 'project_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('client_project');
